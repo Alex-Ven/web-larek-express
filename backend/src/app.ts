40b4f0ebb4adc680440a36fc28235062c37377bf
Alex_V <venedykhin@yandex.ru>
2025-04-29 13:41:59 +1200

    Подключение к базе данных MongoDB

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,33 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
 
 const app = express();
-app.get('/product', (req, res) => {res.send([]); });
-app.listen(3000, () => { console.log('listerning on port 3000'); });
+const PORT = 3000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Подключение к MongoDB
+const connectToMongoDB = async () => {
+  try {
+    await mongoose.connect('mongodb://127.0.0.1:27017/weblarek');
+    console.log('Подключено к базе данных MongoDB');
+  } catch (error) {
+    console.error('Ошибка подключения к базе данных:', error);
+    process.exit(1); // Завершаем процесс при ошибке подключения
+  }
+};
+
+connectToMongoDB();
+
+// Простой маршрут для проверки работы сервера
+app.get('/', (req, res) => {
+  res.send('Express-сервер запущен!');
+});
+
+// Запуск сервера
+app.listen(PORT, () => {
+  console.log(`Сервер запущен на http://localhost:${PORT}`);
+});
